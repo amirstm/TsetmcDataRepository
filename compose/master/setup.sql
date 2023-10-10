9d1db76f06698706a838fd888a16eb44d9fd6006
AmirSTM <amirstm@gmail.com>
2023-10-10 16:26:16 +0330

    Change naming conventions for database

--- compose/master/setup.sql
+++ compose/master/setup.sql
@@ -1,128 +1,128 @@
-DROP DATABASE IF EXISTS TseMarket;
-CREATE DATABASE TseMarket;
-Use TseMarket;
+DROP DATABASE IF EXISTS tse_market;
+CREATE DATABASE tse_market;
+Use tse_market;
 
-GRANT ALL PRIVILEGES ON TseMarket.* TO 'master'@'%';
+GRANT ALL PRIVILEGES ON tse_market.* TO 'master'@'%';
 
-DROP TABLE IF EXISTS InstrumentIdentification;
-DROP TABLE IF EXISTS InstrumentType;
-DROP TABLE IF EXISTS ExchangeMarket;
-DROP TABLE IF EXISTS IndustrySubSector;
-DROP TABLE IF EXISTS IndustrySector;
-DROP TABLE IF EXISTS RealtimeTsetmcNatlegContribution;
-DROP TABLE IF EXISTS DailyTradeCandle;
-DROP TABLE IF EXISTS DailyNatlegContribution;
+DROP TABLE IF EXISTS instrument_identification;
+DROP TABLE IF EXISTS instrument_type;
+DROP TABLE IF EXISTS exchange_market;
+DROP TABLE IF EXISTS industry_sub_sector;
+DROP TABLE IF EXISTS industry_sector;
+DROP TABLE IF EXISTS realtime_tsetmc_natleg_contribution;
+DROP TABLE IF EXISTS daily_trade_candle;
+DROP TABLE IF EXISTS daily_natleg_contribution;
 
-CREATE TABLE IndustrySector(
-	Id INT NOT NULL,
-	Title NVARCHAR(64) NOT NULL,
-	CONSTRAINT PK_IndustrySector PRIMARY KEY (Id)
+CREATE TABLE industry_sector(
+	id INT NOT NULL,
+	title NVARCHAR(64) NOT NULL,
+	CONSTRAINT pk_industry_sector PRIMARY KEY (id)
 );
 
-CREATE TABLE IndustrySubSector(
-	Id INT NOT NULL,
-	Title NVARCHAR(64) NOT NULL,
-	IndustrySectorId INT NOT NULL,
-	CONSTRAINT PK_IndustrySubSector PRIMARY KEY (Id),
-	CONSTRAINT FK_IndustrySector_IndustrySubSector FOREIGN KEY (IndustrySectorId) REFERENCES IndustrySector(Id)
+CREATE TABLE industry_sub_sector(
+	id INT NOT NULL,
+	title NVARCHAR(64) NOT NULL,
+	industry_sector_id INT NOT NULL,
+	CONSTRAINT pk_industry_sub_sector PRIMARY KEY (id),
+	CONSTRAINT fk_industry_sector_industry_sub_sector FOREIGN KEY (industry_sector_id) REFERENCES industry_sector(id)
 );
 
-CREATE TABLE ExchangeMarket(
-	Id INT NOT NULL,
-	Title NVARCHAR(64) NOT NULL,
-	CONSTRAINT PK_ExchangeMarket PRIMARY KEY (Id)
+CREATE TABLE exchange_market(
+	id INT NOT NULL,
+	title NVARCHAR(64) NOT NULL,
+	CONSTRAINT pk_exchange_market PRIMARY KEY (id)
 );
 
-CREATE TABLE InstrumentType(
-	Id INT NOT NULL,
-	Title NVARCHAR(128) NOT NULL,
-	CONSTRAINT PK_InstrumentType PRIMARY KEY (Id)
+CREATE TABLE instrument_type(
+	id INT NOT NULL,
+	title NVARCHAR(128) NOT NULL,
+	CONSTRAINT pk_instrument_type PRIMARY KEY (id)
 );
 
-CREATE TABLE InstrumentIdentification(
-	Isin NCHAR(12) NOT NULL,
-	TsetmcCode NVARCHAR(32) NOT NULL,
-	Ticker NVARCHAR(32) NOT NULL,
-	PersianName NVARCHAR(64) NULL,
-	EnglishName NVARCHAR(64) NULL,
-	InstrumentTypeId INT NOT NULL,
-	ExchangeMarketId INT NULL,
-	IndustrySubSectorId INT NULL,
-	CONSTRAINT PK_InstrumentIdentification PRIMARY KEY (Isin),
-	CONSTRAINT UQ_InstrumentIdentification_TsetmcCode UNIQUE (TsetmcCode),
-	CONSTRAINT FK_Identification_Type FOREIGN KEY (InstrumentTypeId) REFERENCES InstrumentType(Id),
-	CONSTRAINT FK_Identification_TsetmcExchangeMarket FOREIGN KEY (ExchangeMarketId) REFERENCES ExchangeMarket(Id),
-	CONSTRAINT FK_Identification_TsetmcIndustrySubSector FOREIGN KEY (IndustrySubSectorId) REFERENCES IndustrySubSector(Id)
+CREATE TABLE instrument_identification(
+	isin NCHAR(12) NOT NULL,
+	tsetmc_code NVARCHAR(32) NOT NULL,
+	ticker NVARCHAR(32) NOT NULL,
+	persian_name NVARCHAR(64) NULL,
+	english_name NVARCHAR(64) NULL,
+	instrument_type_id INT NOT NULL,
+	exchange_market_id INT NULL,
+	industry_sub_sector_id INT NULL,
+	CONSTRAINT pk_instrument_identification PRIMARY KEY (isin),
+	CONSTRAINT uq_instrument_identification_tsetmc_code UNIQUE (tsetmc_code),
+	CONSTRAINT fk_identification_type FOREIGN KEY (instrument_type_id) REFERENCES instrument_type(id),
+	CONSTRAINT fk_identification_tsetmc_exchange_market FOREIGN KEY (exchange_market_id) REFERENCES exchange_market(id),
+	CONSTRAINT fk_identification_tsetmc_industry_sub_sector FOREIGN KEY (industry_sub_sector_id) REFERENCES industry_sub_sector(id)
 );
 
-CREATE TABLE IndexIdentification(
-	Isin NCHAR(12) NOT NULL,
-	TsetmcCode NVARCHAR(32) NOT NULL,
-	PersianName NVARCHAR(64) NULL,
-	EnglishName NVARCHAR(64) NULL,
-	CONSTRAINT PK_IndexIdentification PRIMARY KEY (Isin),
-	CONSTRAINT UQ_IndexIdentification_TsetmcCode UNIQUE (TsetmcCode)
+CREATE TABLE index_identification(
+	isin NCHAR(12) NOT NULL,
+	tsetmc_code NVARCHAR(32) NOT NULL,
+	persian_name NVARCHAR(64) NULL,
+	english_name NVARCHAR(64) NULL,
+	CONSTRAINT pk_index_identification PRIMARY KEY (isin),
+	CONSTRAINT uq_index_identification_tsetmc_code UNIQUE (tsetmc_code)
 );
 
-CREATE TABLE DailyTradeCandle(
-	Isin NCHAR(12) NOT NULL,
-	RecordDate DATE NOT NULL,
-	PreviousPrice BIGINT,
-	OpenPrice BIGINT,
-	ClosePrice BIGINT,
-	LastPrice BIGINT,
-	MaxPrice BIGINT,
-	MinPrice BIGINT,
-	TradeNum INT,
-	TradeVolume BIGINT,
-	TradeValue BIGINT,
-	CONSTRAINT PK_DailyTradeCandle PRIMARY KEY (Isin),
-	CONSTRAINT FK_DailyTradeCandle_InstrumentIdentification FOREIGN KEY (Isin) REFERENCES InstrumentIdentification(Isin)	
+CREATE TABLE daily_trade_candle(
+	isin NCHAR(12) NOT NULL,
+	record_date DATE NOT NULL,
+	previous_price BIGINT,
+	open_price BIGINT,
+	close_price BIGINT,
+	last_price BIGINT,
+	max_price BIGINT,
+	min_price BIGINT,
+	trade_num INT,
+	trade_volume BIGINT,
+	trade_value BIGINT,
+	CONSTRAINT pk_daily_trade_candle PRIMARY KEY (isin),
+	CONSTRAINT fk_daily_trade_candle_instrument_identification FOREIGN KEY (isin) REFERENCES instrument_identification(Isin)	
 );
 
-CREATE TABLE DailyClientType(
-	Isin NCHAR(12) NOT NULL,
-	RecordDate DATE NOT NULL,
-	NaturalBuyNum INT NOT NULL,
-	LegalBuyNum INT NOT NULL,
-	NaturalBuyVolume BIGINT NOT NULL,
-	LegalBuyVolume BIGINT NOT NULL,
-	NaturalSellNum INT NOT NULL,
-	LegalSellNum INT NOT NULL,
-	NaturalSellVolume BIGINT NOT NULL,
-	LegalSellVolume BIGINT NOT NULL,
-	CONSTRAINT PK_DailyClientType PRIMARY KEY (Isin),
-	CONSTRAINT FK_DailyClientType_InstrumentIdentification FOREIGN KEY (Isin) REFERENCES InstrumentIdentification(Isin)
+CREATE TABLE daily_client_type(
+	isin NCHAR(12) NOT NULL,
+	record_date DATE NOT NULL,
+	natural_buy_num INT NOT NULL,
+	legal_buy_num INT NOT NULL,
+	natural_buy_volume BIGINT NOT NULL,
+	legal_buy_volume BIGINT NOT NULL,
+	natural_sell_num INT NOT NULL,
+	legal_sell_num INT NOT NULL,
+	natural_sell_volume BIGINT NOT NULL,
+	legal_sell_volume BIGINT NOT NULL,
+	CONSTRAINT pk_daily_client_type PRIMARY KEY (isin),
+	CONSTRAINT fk_daily_client_type_instrument_identification FOREIGN KEY (isin) REFERENCES instrument_identification(isin)
 );
 
-CREATE TABLE DailyInstrumentDetail(
-	Isin NCHAR(12) NOT NULL,
-	RecordDate DATE NOT NULL,
-	TotalShareCount BIGINT,
-	BaseVolume BIGINT,
-	MaxPriceThreshold BIGINT,
-	MinPriceThreshold BIGINT,
-	CONSTRAINT PK_DailyInstrumentDetail PRIMARY KEY (Isin),
-	CONSTRAINT FK_DailyInstrumentDetail_InstrumentIdentification FOREIGN KEY (Isin) REFERENCES InstrumentIdentification(Isin)
+CREATE TABLE daily_instrument_detail(
+	isin NCHAR(12) NOT NULL,
+	record_date DATE NOT NULL,
+	total_share_count BIGINT,
+	base_volume BIGINT,
+	max_price_threshold BIGINT,
+	min_price_threshold BIGINT,
+	CONSTRAINT pk_daily_instrument_detail PRIMARY KEY (isin),
+	CONSTRAINT fk_daily_instrument_detail_instrument_identification FOREIGN KEY (isin) REFERENCES instrument_identification(isin)
 );
 
-CREATE TABLE TickTrade(
-	Isin NCHAR(12) NOT NULL,
-	RecordDateTime DATETIME NOT NULL,
-	Htn INT NOT NULL,
-	Quantity BIGINT NOT NULL,
-	Price BIGINT NOT NULL,
-	Invalidated BIT NOT NULL DEFAULT 0,
-	CONSTRAINT PK_TickTrade PRIMARY KEY (Isin),
-	CONSTRAINT FK_TickTrade_InstrumentIdentification FOREIGN KEY (Isin) REFERENCES InstrumentIdentification(Isin)
+CREATE TABLE tick_trade(
+	isin NCHAR(12) NOT NULL,
+	record_date_time DATETIME NOT NULL,
+	htn INT NOT NULL,
+	quantity BIGINT NOT NULL,
+	price BIGINT NOT NULL,
+	invalidated BIT NOT NULL DEFAULT 0,
+	CONSTRAINT pk_tick_trade PRIMARY KEY (isin),
+	CONSTRAINT fk_tick_trade_instrument_identification FOREIGN KEY (isin) REFERENCES instrument_identification(Isin)
 );
 
-CREATE TABLE DailyIndexValue(
-	Isin NCHAR(12) NOT NULL,
-	RecordDate DATE NOT NULL,
-	CloseVal FLOAT NOT NULL, 
-	MaxVal FLOAT NOT NULL,
-	MinVal FLOAT NOT NULL,
-	CONSTRAINT PK_DailyIndexValue PRIMARY KEY (Isin),
-	CONSTRAINT FK_DailyIndexValue_IndexIdentification FOREIGN KEY (Isin) REFERENCES IndexIdentification(Isin)
+CREATE TABLE daily_index_value(
+	isin NCHAR(12) NOT NULL,
+	record_date DATE NOT NULL,
+	close_value FLOAT NOT NULL, 
+	max_value FLOAT NOT NULL,
+	min_value FLOAT NOT NULL,
+	CONSTRAINT pk_daily_index_value PRIMARY KEY (isin),
+	CONSTRAINT fk_daily_index_value_index_identification FOREIGN KEY (isin) REFERENCES index_identification(isin)
 );
--- tse-repo-operations/python/sql.py
+++ tse-repo-operations/python/sql.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from sqlalchemy import ForeignKey
 from typing import List
 from sqlalchemy.orm import relationship
@@ -5,7 +6,7 @@ from typing import Optional
 from sqlalchemy import String
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import mapper, sessionmaker
 from dotenv import load_dotenv
@@ -33,42 +34,41 @@ mysql_connector = "mysql+mysqlconnector://{}:{}@{}:{}/{}".format(
     MYSQL_PORT,
     MYSQL_DB
 )
-print(mysql_connector)
 engine = create_engine(
     mysql_connector,
-    echo=True
+    echo=False
 )
 
 
+@dataclass
 class InstrumentType(Base):
-    __tablename__ = "InstrumentType"
+    __tablename__ = "instrument_type"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(32))
 
-    # instrument_identifications: Mapped[List["InstrumentIdentification"]] = relationship(
-    #     back_populates="InstrumentType", cascade="all, delete-orphan"
-    # )
+    instrument_identifications: Mapped[list["InstrumentIdentification"]] = relationship(
+        back_populates="instrument_type", cascade="all, delete-orphan"
+    )
 
     def __repr__(self) -> str:
-        return f"InstrumentType(id={self.id!r}, title={self.title!r})"
+        return f"InstrumentType(id={self.id}, title={self.title})"
 
 
+@dataclass
 class InstrumentIdentification(Base):
-    __tablename__ = "InstrumentIdentification"
+    __tablename__ = "instrument_identification"
 
     isin: Mapped[str] = mapped_column(String(length=12), primary_key=True)
-    tsetmc_code: Mapped[str] = mapped_column(String(32), name="TsetmcCode")
+    tsetmc_code: Mapped[str] = mapped_column(String(32))
     ticker: Mapped[str] = mapped_column(String(32))
 
-    # instrument_type_id: Mapped[int] = mapped_column(
-    #     ForeignKey("InstrumentType.id"))
+    instrument_type_id: Mapped[int] = mapped_column(
+        ForeignKey("instrument_type.id")
+    )
 
-    # InstrumentType: Mapped["InstrumentType"] = relationship(
-    #     back_populates="InstrumentIdentification")
-
-    def __repr__(self) -> str:
-        return f"InstrumentIdentification(isin={self.isin!r})"
+    instrument_type: Mapped[InstrumentType] = relationship(
+    )
 
 
 def loadSession():
@@ -81,7 +81,10 @@ def loadSession():
 
 if __name__ == "__main__":
     session = loadSession()
-    res = session.query(InstrumentType).all()
-    print(res[0])
-    res = session.query(InstrumentIdentification).all()
-    print(res[0])
+    # res = session.query(InstrumentType).all()
+    # print(res[0])
+    # res = session.query(InstrumentIdentification).all()
+    # print(res[0])
+    res = session.query(InstrumentType).where(
+        InstrumentType.id == 12345).first()
+    print(res.instrument_identifications)
