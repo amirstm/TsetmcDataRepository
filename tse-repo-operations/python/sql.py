70cb55016fac9109786177d77e9a596e29f25a29
AmirSTM <amirstm@gmail.com>
2023-10-10 18:25:38 +0330

    Map leftover database tables to python classes

--- tse-repo-operations/python/sql.py
+++ tse-repo-operations/python/sql.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from typing import Optional
-from sqlalchemy.types import NCHAR, NVARCHAR
+from sqlalchemy.types import NCHAR, NVARCHAR, BIGINT
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import declarative_base
@@ -11,6 +11,7 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import mapper, sessionmaker
 from dotenv import load_dotenv
 import os
+from datetime import date, datetime
 
 load_dotenv()
 
@@ -143,14 +144,121 @@ class IndexIdentification(Base):
     english_name: Mapped[Optional[str]] = mapped_column(NVARCHAR(64))
 
 
-# @dataclass
-# class DailyTradeCandle(Base):
-#     __tablename__ = "index_identification"
+@dataclass
+class DailyTradeCandle(Base):
+    __tablename__ = "daily_trade_candle"
 
-#     isin: Mapped[str] = mapped_column(NCHAR(12), primary_key=True)
-#     tsetmc_code: Mapped[str] = mapped_column(NVARCHAR(32))
-#     persian_name: Mapped[Optional[str]] = mapped_column(NVARCHAR(64))
-#     english_name: Mapped[Optional[str]] = mapped_column(NVARCHAR(64))
+    daily_trade_candle_id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True
+    )
+    isin: Mapped[str] = mapped_column(
+        ForeignKey("instrument_identification.isin")
+    )
+    record_date: Mapped[date] = mapped_column()
+    previous_price: Mapped[int] = mapped_column(BIGINT())
+    open_price: Mapped[int] = mapped_column(BIGINT())
+    close_price: Mapped[int] = mapped_column(BIGINT())
+    last_price: Mapped[int] = mapped_column(BIGINT())
+    max_price: Mapped[int] = mapped_column(BIGINT())
+    min_price: Mapped[int] = mapped_column(BIGINT())
+    trade_num: Mapped[int] = mapped_column()
+    trade_volume: Mapped[int] = mapped_column(BIGINT())
+    trade_value: Mapped[int] = mapped_column(BIGINT())
+
+    instrument_identification: Mapped[InstrumentIdentification] = relationship(
+    )
+
+
+@dataclass
+class DailyClientType(Base):
+    __tablename__ = "daily_client_type"
+
+    daily_client_type_id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True
+    )
+    isin: Mapped[str] = mapped_column(
+        ForeignKey("instrument_identification.isin")
+    )
+    record_date: Mapped[date] = mapped_column()
+    natural_buy_num: Mapped[int] = mapped_column()
+    legal_buy_num: Mapped[int] = mapped_column()
+    natural_buy_value: Mapped[int] = mapped_column(BIGINT())
+    legal_buy_value: Mapped[int] = mapped_column(BIGINT())
+    natural_buy_volume: Mapped[int] = mapped_column(BIGINT())
+    legal_buy_volume: Mapped[int] = mapped_column(BIGINT())
+    natural_sell_num: Mapped[int] = mapped_column()
+    legal_sell_num: Mapped[int] = mapped_column()
+    natural_sell_value: Mapped[int] = mapped_column(BIGINT())
+    legal_sell_value: Mapped[int] = mapped_column(BIGINT())
+    natural_sell_volume: Mapped[int] = mapped_column(BIGINT())
+    legal_sell_volume: Mapped[int] = mapped_column(BIGINT())
+
+    instrument_identification: Mapped[InstrumentIdentification] = relationship(
+    )
+
+
+@dataclass
+class DailyInstrumentDetail(Base):
+    __tablename__ = "daily_instrument_detail"
+
+    daily_instrument_detail_id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True
+    )
+    isin: Mapped[str] = mapped_column(
+        ForeignKey("instrument_identification.isin")
+    )
+    record_date: Mapped[date] = mapped_column()
+    total_share_count: Mapped[int] = mapped_column(BIGINT())
+    base_volume: Mapped[int] = mapped_column(BIGINT())
+    max_price_threshold: Mapped[int] = mapped_column(BIGINT())
+    min_price_threshold: Mapped[int] = mapped_column(BIGINT())
+
+    instrument_identification: Mapped[InstrumentIdentification] = relationship(
+    )
+
+
+@dataclass
+class TickTrade(Base):
+    __tablename__ = "tick_trade"
+
+    tick_trade_id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True
+    )
+    isin: Mapped[str] = mapped_column(
+        ForeignKey("instrument_identification.isin")
+    )
+    record_date_time: Mapped[datetime] = mapped_column()
+    htn: Mapped[int] = mapped_column()
+    quantity: Mapped[int] = mapped_column(BIGINT())
+    price: Mapped[int] = mapped_column(BIGINT())
+    invalidated: Mapped[bool] = mapped_column(default=False)
+
+    instrument_identification: Mapped[InstrumentIdentification] = relationship(
+    )
+
+
+@dataclass
+class DailyIndexValue(Base):
+    __tablename__ = "daily_index_value"
+
+    daily_index_value_id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True
+    )
+    isin: Mapped[str] = mapped_column(
+        ForeignKey("instrument_identification.isin")
+    )
+    record_date: Mapped[date] = mapped_column()
+    close_value: Mapped[float] = mapped_column()
+    max_value: Mapped[float] = mapped_column()
+    min_value: Mapped[float] = mapped_column()
+
+    instrument_identification: Mapped[InstrumentIdentification] = relationship(
+    )
 
 
 def loadSession():
