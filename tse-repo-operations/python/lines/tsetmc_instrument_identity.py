2a5f94d2ef1bda5436e2a11c05fd77cdab91c285
AmirSTM <amirstm@gmail.com>
2023-10-13 09:11:05 +0330

    Draw outline for TsetmcInstrumentIdentity class

--- tse-repo-operations/python/lines/tsetmc_instrument_identity.py
+++ tse-repo-operations/python/lines/tsetmc_instrument_identity.py
@@ -20,3 +20,23 @@ from models.tse_market import (
 class JobDescription(line.JobDescription):
     """Overriden JobDescription for module tsetmc_instrument_identity"""
     search_by: str = None
+
+
+class TsetmcInstrumentIdentity(line.Worker):
+    """Overriden worker for module tsetmc_instrument_identity"""
+
+    async def perform_task(self, job_description: JobDescription) -> line.JobReport:
+        """Performs the task using the provided job description"""
+        return _Shift(job_description=job_description).perform_task()
+
+
+class _Shift:
+    """Internal class for simpler performance of the worker's task"""
+
+    def __init__(self, job_description: JobDescription):
+        self.job_description: JobDescription = job_description
+        self.report: line.JobReport = line.JobReport()
+
+    def perform_task(self) -> line.JobReport:
+
+        return self.report
