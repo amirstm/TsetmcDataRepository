ca6c465b9caa91f4f1f09ceb8f5b2265d3f7d48f
AmirSTM <amirstm@gmail.com>
2023-10-16 15:09:24 +0330

    Adding new sectors, sub sectors, and markets in tsetmc_instrument_searcher

--- tse-repo-operations/python/lines/tsetmc_instrument_searcher.py
+++ tse-repo-operations/python/lines/tsetmc_instrument_searcher.py
@@ -11,6 +11,9 @@ from models.tse_market import (
     get_tse_market_session,
     InstrumentIdentification,
     InstrumentType,
+    IndustrySector,
+    IndustrySubSector,
+    ExchangeMarket,
 )
 
 
@@ -59,44 +62,155 @@ class _Shift:
         return self.report
 
     def insert_to_identifications(
-        self, new_identifications: list[InstrumentIdentification]
+        self, new_identifications: list[tsetmc.InstrumentIdentification]
     ) -> None:
         """Inserts new records to database"""
         with get_tse_market_session() as session:
             instrument_types = session.query(InstrumentType).all()
+            old_sectors = session.query(IndustrySector).all()
+            old_sub_sectors = session.query(IndustrySubSector).all()
+            old_exchange_markets = session.query(ExchangeMarket).all()
             unknown_type_instruments = [
                 x
                 for x in new_identifications
                 if not any(
-                    y
-                    for y in instrument_types
-                    if y.instrument_type_id == x.instrument_type_id
+                    y for y in instrument_types if y.instrument_type_id == x.type_id
                 )
             ]
             if unknown_type_instruments:
-                set_unknown = {x.instrument_type_id for x in unknown_type_instruments}
+                set_unknown = {x.type_id for x in unknown_type_instruments}
                 self.report.information.append(f"Unknown types ➡️ {set_unknown!r}")
                 self.report.warnings.append(repr(unknown_type_instruments))
             instruments_to_add = [
-                x
-                for x in new_identifications
-                if any(
-                    y
-                    for y in instrument_types
-                    if y.instrument_type_id == x.instrument_type_id
+                InstrumentIdentification(
+                    isin=x.isin,
+                    tsetmc_code=x.tsetmc_code,
+                    name_persian=x.name_persian,
+                    name_english=x.name_english,
+                    ticker=x.ticker,
+                    exchange_market_id=x.market_code,
+                    industry_sub_sector_id=x.sub_sector_code,
+                    instrument_type_id=x.type_id,
                 )
+                for x in new_identifications
+                if any(y for y in instrument_types if y.instrument_type_id == x.type_id)
             ]
+            session.add_all(self.get_new_sectors(new_identifications, old_sectors))
+            session.add_all(
+                self.get_new_sub_sectors(new_identifications, old_sub_sectors)
+            )
+            session.add_all(
+                self.get_new_exchange_markets(new_identifications, old_exchange_markets)
+            )
             session.add_all(instruments_to_add)
             session.commit()
             self.report.information.append(
                 f"Inserted instruments ➡️ {len(instruments_to_add)}",
             )
 
+    def get_new_exchange_markets(
+        self,
+        identifications: list[tsetmc.InstrumentIdentification],
+        old_exchange_markets: list[ExchangeMarket],
+    ) -> list[ExchangeMarket]:
+        """Gets new exchange markets that should be added to the database"""
+        all_exchange_markets: list[ExchangeMarket] = []
+        for identification in identifications:
+            if not any(
+                x
+                for x in all_exchange_markets
+                if x.exchange_market_id == identification.market_code
+            ):
+                all_exchange_markets.append(
+                    ExchangeMarket(
+                        exchange_market_id=identification.market_code,
+                        title=identification.market_title,
+                    )
+                )
+        new_exchange_markets = [
+            x
+            for x in all_exchange_markets
+            if not any(
+                y
+                for y in old_exchange_markets
+                if y.exchange_market_id == x.exchange_market_id
+            )
+        ]
+        self.report.information.append(
+            f"Inserted exchange markets ➡️ {len(new_exchange_markets)}",
+        )
+        return new_exchange_markets
+
+    def get_new_sub_sectors(
+        self,
+        identifications: list[tsetmc.InstrumentIdentification],
+        old_sub_sectors: list[IndustrySubSector],
+    ) -> list[IndustrySubSector]:
+        """Gets new sub sectors that should be added to the database"""
+        all_sub_sectors: list[IndustrySubSector] = []
+        for identification in identifications:
+            if not any(
+                x
+                for x in all_sub_sectors
+                if x.industry_sub_sector_id == identification.sub_sector_code
+            ):
+                all_sub_sectors.append(
+                    IndustrySubSector(
+                        industry_sub_sector_id=identification.sub_sector_code,
+                        title=identification.sub_sector_title,
+                        industry_sector_id=identification.sector_code,
+                    )
+                )
+        new_sub_sectors = [
+            x
+            for x in all_sub_sectors
+            if not any(
+                y
+                for y in old_sub_sectors
+                if y.industry_sub_sector_id == x.industry_sub_sector_id
+            )
+        ]
+        self.report.information.append(
+            f"Inserted sub sectors ➡️ {len(new_sub_sectors)}",
+        )
+        return new_sub_sectors
+
+    def get_new_sectors(
+        self,
+        identifications: list[tsetmc.InstrumentIdentification],
+        old_sectors: list[IndustrySector],
+    ) -> list[IndustrySector]:
+        """Gets new sectors that should be added to the database"""
+        all_sectors: list[IndustrySector] = []
+        for identification in identifications:
+            if not any(
+                x
+                for x in all_sectors
+                if x.industry_sector_id == identification.sector_code
+            ):
+                all_sectors.append(
+                    IndustrySector(
+                        industry_sector_id=identification.sector_code,
+                        title=identification.sector_title,
+                    )
+                )
+        new_sectors = [
+            x
+            for x in all_sectors
+            if not any(
+                y for y in old_sectors if y.industry_sector_id == x.industry_sector_id
+            )
+        ]
+        self.report.information.append(
+            f"Inserted sectors ➡️ {len(new_sectors)}",
+        )
+        return new_sectors
+
     async def get_instrument_identifications(
         self, search_results: list[tsetmc.InstrumentSearchItem]
-    ) -> list[InstrumentIdentification]:
+    ) -> list[tsetmc.InstrumentIdentification]:
         """Gets identification from Tsetmc for new instruments"""
-        results: list[InstrumentIdentification] = []
+        results: list[tsetmc.InstrumentIdentification] = []
         async with tsetmc.TsetmcScraper() as scraper:
             for search_result in search_results:
                 try:
@@ -107,18 +221,7 @@ class _Shift:
                     instrument_identity = await scraper.get_instrument_identity(
                         tsetmc_code=search_result.tsetmc_code
                     )
-                    results.append(
-                        InstrumentIdentification(
-                            isin=instrument_identity.isin,
-                            tsetmc_code=instrument_identity.tsetmc_code,
-                            name_persian=instrument_identity.name_persian,
-                            name_english=instrument_identity.name_english,
-                            ticker=instrument_identity.ticker,
-                            exchange_market_id=instrument_identity.market_code,
-                            industry_sub_sector_id=instrument_identity.sector_code,
-                            instrument_type_id=instrument_identity.type_id,
-                        )
-                    )
+                    results.append(instrument_identity)
                 except (httpx.RequestError, tsetmc.TsetmcScrapeException):
                     self.report.warnings.append(
                         f"Getting instrument identity for [{search_result}] failed.",
